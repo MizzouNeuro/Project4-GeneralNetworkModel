b04fed1ea47303e7c7f28257ae85b726be3aae84
Benjamin Latimer <latimerb@lewis4-r710-login-node223.cobalt.lan>
2018-10-15 12:26:34 -0500

    works on Lewis now

--- batch_lewis.sh
+++ batch_lewis.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-#SBATCH -p Lewis,RC
+#SBATCH -p Lewis,hpc5
 #SBATCH -N 1
-#SBATCH -A rc
-#SBATCH -n 2
+#SBATCH -n 10
 #SBATCH --qos=normal
-#SBATCH --job-name=ca1
-#SBATCH --output=ca1%j.out
-#SBATCH --licenses=matlab:1
+#SBATCH --job-name=BLA
+#SBATCH --output=bla%j.out
 #SBATCH --time 0-00:30
 
-module load matlab/matlab-R2018a
 module load intel/intel-2016-update2
 module load nrn/nrn-mpi-7.4
 module load openmpi/openmpi-2.0.0
@@ -18,6 +15,4 @@ module load openmpi/openmpi-2.0.0
 module list
 echo "Starting model at $(date)"
 
-#srun -N 1 -n 1 -c 1 --mem 11G matlab -nodesktop -nosplash -nodisplay -r "run('./input/connectivity.m');exit"
-
-mpirun --mca mpi_warn_on_fork 0 nrniv -mpi main.hoc
+mpirun nrniv -mpi main.hoc
